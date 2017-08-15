d1aab2d80679ed8a0e17ef22c91fffbb1e151a7f
m4l1c3 <jon@jonsomnia.com>
2017-08-15 08:37:24 -0600

    Adding logger, updating subprocess.call calls

--- modules/operating_system.py
+++ modules/operating_system.py
@@ -2,6 +2,7 @@
 Operating system enumeration
 """
 import subprocess
+from modules.logger import Logger
 
 """
 Class Operating system
@@ -11,52 +12,42 @@ class OperatingSystem(object):
     constructor
     """
     def __init__(self):
+        self.logger = Logger()
         return
 
     def get_issues(self):
         """
         get linux issue file
         """
-        print "[*] Grabbing /etc/issue"
-        return subprocess.call("cat /etc/issue")
+        self.logger.normal_output("[*] Grabbing /etc/issue")
+        return subprocess.call(["cat /etc/issue"])
 
     def get_releases(self):
         """
         get os releases
         """
-        print "[*] Grabbing releases"
+        self.logger.normal_output("[*] Grabbing releases")
         return subprocess.call("cat /etc/*-release")
-        # cat /etc/lsb-release      # Debian based
-        # cat /etc/redhat-release   # Redhat based
 
     def get_kernel(self):
         """
         get kernel info
         """
-        print "[*] Grabbing Kernel information"
-        kernel = subprocess.call("cat /proc/version")
-        kernel += subprocess.call("uname -a")
-        kernel += subprocess.call("uname -mrs")
-        kernel += subprocess.call("rpm -q kernel")
-        kernel += subprocess.call("dmesg | grep Linux")
-        kernel += subprocess.call("ls /boot | grep vmlinuz-")
+        self.logger.normal_output("[*] Grabbing Kernel information")
+        kernel = subprocess.call(["cat /proc/version", "uname -a", "uname -mrs", "rpm -q kernel", "dmesg | grep Linux", "ls /boot | grep vmlinuz-"])
+        
         return kernel
 
     def get_environment(self):
         """
         get system environment variables
         """
-        environment = subprocess.call("cat /etc/profile")
-        environment += subprocess.call("cat /etc/bashrc")
-        environment += subprocess.call("cat ~/.bash_profile")
-        environment += subprocess.call("cat ~/.bashrc")
-        environment += subprocess.call("cat ~/.bash_logout")
-        environment += subprocess.call("env")
-        environment += subprocess.call("set")
-        return environment
+        self.logger.normal_output("[*] Grabbing environment variables")
+        return subprocess.call(["cat /etc/profile", "cat /etc/bashrc", "cat ~/.bash_profile", "cat ~/.bashrc", "cat ~/.bash_logout", "env", "set"])
 
     def get_printers(self):
         """
         get any printers
         """
-        return subprocess.call("lpstat -a")
+        self.logger.normal_output("[*] Grabbing printers")
+        return subprocess.call(["lpstat -a"])
