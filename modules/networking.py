6fd02c281b4622324fcd1fc78750b526c3bfd13e
m4l1c3 <jon@jonsomnia.com>
2017-08-16 18:27:21 -0600

    Updating networking to match "new" setup

--- modules/networking.py
+++ modules/networking.py
@@ -1,7 +1,8 @@
 """
 Networking module
 """
-import subprocess
+import os
+from modules.logger import Logger
 
 class Networking(object):
     """
@@ -10,26 +11,37 @@ class Networking(object):
     def __init__(self):
         return
 
+    def run(self):
+        self.logger = Logger()
+        self.nics()
+        self.get_tcpdump()
+        self.get_networking_config_info()
+        self.get_cached_network_info()
+        self.get_users_and_hosts()
+
     def nics(self):
         """
         get network interfaces
         """
-        nics = subprocess.call("/sbin/ifconfig -a")
-        nics += subprocess.call("cat /etc/network/interfaces")
-        nics += subprocess.call("cat /etc/sysconfig/network")
+        self.logger.normal_output("Running network checks")
+        nics = {}
+        nics.update({"ifconfig": os.system("/sbin/ifconfig -a")})
+        nics.update({"interfaces": os.system("cat /etc/network/interfaces")})
+        nics.update({"network": os.system("cat /etc/sysconfig/network")})
         return nics
 
     def get_networking_config_info(self):
         """
         get networking configuration info, dhcp, etc.
         """
-
-        network_config = subprocess.call("cat /etc/resolv.conf")
-        network_config += subprocess.call("cat /etc/sysconfig/network")
-        network_config += subprocess.call("cat /etc/networks")
-        network_config += subprocess.call("iptables -L")
-        network_config += subprocess.call("hostname")
-        network_config += subprocess.call("dnsdomainname")
+        self.logger.normal_output("Running network configuration checks")
+        network_config = {}
+        network_config.update({"resolv.conf": os.system("cat /etc/resolv.conf")})
+        network_config.update({"sysconfig/network": os.system("cat /etc/sysconfig/network")})
+        network_config.update({"networks": os.system("cat /etc/networks")})
+        network_config.update({"iptables": os.system("iptables -L")})
+        network_config.update({"hostname": os.system("hostname")})
+        network_config.update({"dnsdomainname": os.system("dnsdomainname")})
 
         return network_config
 
@@ -37,29 +49,36 @@ class Networking(object):
         """
         get users and hosts communicating with the system
         """
-        users_and_hosts = subprocess.call("lsof -i")
-        users_and_hosts += subprocess.call("lsof -i :80")
-        users_and_hosts += subprocess.call("grep 80 /etc/services")
-        users_and_hosts += subprocess.call("netstat -antup")
-        users_and_hosts += subprocess.call("netstat -antpx")
-        users_and_hosts += subprocess.call("netstat -tulpn")
-        users_and_hosts += subprocess.call("chkconfig --list")
-        users_and_hosts += subprocess.call("chkconfig --list | grep 3:on")
-        users_and_hosts += subprocess.call("last")
-        users_and_hosts += subprocess.call("w")
+        self.logger.normal_output("Running users and hosts")
+        users_and_hosts = {}
+        users_and_hosts.update({"lsof": os.system("lsof -i")})
+        users_and_hosts.update({"lsof port 80": os.system("lsof -i :80")})
+        users_and_hosts.update({"services on 80": os.system("grep 80 /etc/services")})
+        users_and_hosts.update({"netstat -antup": os.system("netstat -antup")})
+        users_and_hosts.update({"netstat -antpx": os.system("netstat -antpx")})
+        users_and_hosts.update({"netstat - tulpn": os.system("netstat -tulpn")})
+        users_and_hosts.update({"chkconfig --list": os.system("chkconfig --list")})
+        users_and_hosts.update({"chkconfig -- run level and enabled": os.system("chkconfig --list | grep 3:on")})
+        users_and_hosts.update({"last": os.system("last")})
+        users_and_hosts.update({"w": os.system("w")})
         return users_and_hosts
 
     def get_cached_network_info(self):
         """
         get cached networking configuration info
         """
-        cached_network_info = subprocess.call("arp -e")
-        cached_network_info += subprocess.call("route")
-        cached_network_info += subprocess.call("/sbin/route -nee")
+        self.logger.normal_output("Running cached network info")
+        cached_network_info = {}
+        cached_network_info.update({"arp -e": os.system("arp -e")})
+        cached_network_info.update({"route": os.system("route")})
+        cached_network_info.update({"route -nee": os.system("/sbin/route -nee")})
         return cached_network_info
 
     def get_tcpdump(self, host):
         """
         try to get a tcp dump
         """
-        return subprocess.call("tcpdump tcp dst " + host + " 80 and tcp dst 10.5.5.252 21")
+        self.logger.normal_output("Running tcpdump")
+        tcp_dump = {}
+        tcp_dump.update("tcpdump output": os.system("tcpdump tcp dst " + host + " 80 and tcp dst 10.5.5.252 21")})
+        return tcp_dump
