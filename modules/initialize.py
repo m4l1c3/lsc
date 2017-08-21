c125ea2afc944f80bb973069833f32630239c9ba
m4l1c3 <jon@jonsomnia.com>
2017-08-20 21:53:24 -0600

    Adding init module

---
+++ modules/initialize.py
@@ -0,0 +1,31 @@
+"""
+Initialization modules
+"""
+
+class Initialization(object):
+    """
+    Initialization object
+    """
+
+    def __init__(self):
+        return
+
+    def initialize(self):
+        """
+        setup module
+        """
+        return self
+
+    @staticmethod
+    def check_for_config(config_name):
+        """
+        check for existing config of name
+        """
+        return config_name
+
+    @staticmethod
+    def write_config(config):
+        """
+        write newly setup options to a config file
+        """
+        return config
