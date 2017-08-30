ffe1cf0e10cf30015bf305de1b47f2915eb7a448
m4l1c3 <jon@jonsomnia.com>
2017-08-29 22:04:21 -0600

    Getting basic payload generation in place to output a single file, adding to ignore for generated archives/files/etc.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from modules.printers import Printers
 from modules.logger import Logger
 from modules.environments import Environments
 from modules.exfiltrator import Exfiltration
+from modules.payloads import Payloads
 
 class LCS(object):
     """main class"""
@@ -19,6 +20,7 @@ class LCS(object):
     printers = Printers()
     environments = Environments()
     exfiltrator = Exfiltration()
+    payload_generator = Payloads("payload.txt")
 
     def __init__(self):
         self.main()
@@ -34,10 +36,11 @@ class LCS(object):
         """
         default run method
         """
+        # self.payload_generator.generate_payload()
         self.op_sys.run()
-        # self.networking.run()
-        # self.services.run()
-        # self.environments.run()
-        self.exfiltrator.exfiltrate()
+        self.networking.run()
+        self.services.run()
+        self.environments.run()
+        # self.exfiltrator.exfiltrate()
 
 LCS()
--- modules/payloads.py
+++ modules/payloads.py
@@ -1,13 +1,19 @@
 """
 Payloads module
 """
+import glob
+import os
+from modules.logger import Logger
 
 class Payloads(object):
     """
     Payloads object
     """
 
-    def __init__(self):
+    logger = Logger()
+
+    def __init__(self, payload_name):
+        self.payload_name = payload_name
         return
 
     def generate_payload(self):
@@ -16,10 +22,27 @@ class Payloads(object):
         todo have this concat all the files that are generated
         with a config or init script into a single bundle
         """
-        return self
+        try:
+            if os.path.exists("./" + self.payload_name):
+                os.remove("./" + self.payload_name)
+
+            files = [x for x in glob.glob("./modules/*.py") if x != "./modules/__init__.py"]
+
+            with open(self.payload_name, "w") as outfile:
+                for filename in files:
+                    with open(filename) as infile:
+                        for line in infile:
+                            outfile.write(line)
+
+            if os.path.exists("./" + self.payload_name):
+                return True
+        except OSError as error:
+            self.logger.error("Error creating payload: " + error.message)
+
+        return False
 
     def get_payload_type(self):
         """
-        determine what type of payload to generate, php, java, ruby, etc.abs
+        determine what type of payload to generate, php, java, ruby, etc.
         """
         return self
